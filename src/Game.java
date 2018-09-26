bc58db46fb82ee4e4e4668fb3a06a65a430ab13a
Wolfpackzach <43621758+Wolfpackzach@users.noreply.github.com>
2018-09-26 17:07:30 -0400

    Add files via upload

---
+++ src/Game.java
@@ -0,0 +1,11 @@
+/*
+ * File Name:		Game.java
+ * Class:			CMSC 495-6380
+ * Group:			Group 6
+ * Authors:			J. Zach
+ * Purpose:			This class digests the data input into the program and parses it out to different parts of the program.
+ * 					It is also responsible for performing the operations to calculate victories and rankings for the games.
+ */
+public class Game {
+	//Do Stuff
+}
---
+++ src/Tester.java
@@ -0,0 +1,10 @@
+/*
+ * File Name:		Tester.java
+ * Class:			CMSC 495-6380
+ * Group:			Group 6
+ * Authors:			J. Zach
+ * Purpose:			This class will hold the testing materials and code to ensure the program works.
+ */
+public class Tester {
+
+}
