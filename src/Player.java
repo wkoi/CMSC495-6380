f43d78ebe49b76c7d5d6d0d5a59bab804791c7a7
Stephen <Stephen@stephens-macbook-pro.local>
2018-09-26 20:48:59 -0400

    psuedocode
    added pseudocode

--- src/Player.java
+++ src/Player.java
@@ -4,5 +4,59 @@ public class Player {
 	int winCount;
 	int lossCount;
 	int rank;
+	Class Player
+
+	String name;
+	int winCount;
+	int lossCount
+	int rank;
+
+	Player(scanner sc){
+	if has nextString{
+		name = sc.nextString
+	}
+	if has nextInt{
+		rank = sc.nextInt;
+	}	
+	}
+	int getRank(){
+		return rank;
+	}
+
 	
+
+	void setRank(int rank){
+		this.rank = rank;
+	}
+
+	void setwinCount(int winCount){
+		winCount = winCount;
+	}
+
+	void setlossCount
+	(int lossCount){
+		lossCount = lossCount;
+
+	}
+
+	int getwinCount(){
+		return winCount;
+	}
+	int getlossCount
+	(){
+		return lossCount;
+
+	}
+	void resetwinCountlossCount
+	(){
+		winCount = 0;
+		lossCount  = 0
+	}
+	String getName(){
+		return name;
+	}
+	void setName(String newName){
+		newName = name;
+	}
+
 }
