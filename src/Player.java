a5e1e850dd1c23cbce526e14dba4bb80bad87187
Wolfpackzach <43621758+Wolfpackzach@users.noreply.github.com>
2018-09-30 09:42:06 -0400

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,31 +1,31 @@
+/*
+ * File Name:		Player.java
+ * Class:			CMSC 495-6380
+ * Group:			Group 6
+ * Authors:			Will Krohn, Stephen
+ * Date: 			9/29/2018
+ * Purpose: 		This document is responsible for the Player java class for group 6's project to predict wins
+ */
+import java.util.*;
 import java.util.Scanner;
 
-/*This document is responsible for the Player java class for group 6's project to predict wins
- * Author: Will Krohn
- * Version Number(dates): 1.0(9/29/18)
- */
-public class Player {
-import java.util.*;
 public class Player {
 	String name;
-	int winCount;
-	int lossCount;
-	int rank;
-
+	int winCount, lossCount, rank;
+	
 	Player(Scanner sc){
-	if (sc.hasNext()){
-		name = sc.next();
-	}
-	if (sc.hasNext()){
-		rank = sc.nextInt();
-	}	
+		if (sc.hasNext()){
+			name = sc.next();
+		}
+		if (sc.hasNext()){
+			rank = sc.nextInt();
+		}	
 	
 	}
+	
 	int getRank(){
 		return rank;
 	}
-
-	
 
 	void setRank(int rank){
 		this.rank = rank;
@@ -61,57 +61,4 @@ public class Player {
 		newName = name;
 	}
 
-}
-
-	String name;
-	int wins,losses,rank;
-	Player(Scanner sc){
-		if(sc.hasNext()) {
-			name=sc.next();
-			if(sc.hasNextInt()) {
-				rank=sc.nextInt();
-			}
-		}
-		else {
-			//Here for later development for catching bad data
-		}
-	}
-	//Gets Player Name
-	public String getName() {
-		return name;
-	}
-	//Set Player name
-	public void setName(String n) {
-		name=n;
-	}
-	//Gets player Rank
-	public int getRank() {
-		return this.rank;
-	}
-	//Set rank
-	public void setRank(int r) {
-		this.rank=r;
-	}
-	//Set Wins
-	public void setWins(int w) {
-		wins=w;
-	}
-	//Set Losses
-	public void setLosses(int l) {
-		losses=l;
-	}
-	//returns wins
-	public int getWins() {
-		return wins;
-	}
-	//returns losses
-	public int getLosses() {
-		return losses;
-	}
-	//Resets Player Season
-	public void resetSeason(){
-		wins=0;
-		losses=0;
-	}
-	
 }
